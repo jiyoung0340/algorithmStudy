576f8b2bc20cfa01960440807300a1edeb86726d
Jiyoung Oh <jiyoung4061@gmail.com>
2023-10-29 23:25:40 +0900

    코드 수정(추가 수정 필요)

--- 10-4/boj1107.py
+++ 10-4/boj1107.py
@@ -1,14 +1,16 @@
 import sys
 
-n = sys.stdin.readline()
+n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 broken_number = list(map(int, sys.stdin.readline().rstrip().split(' ')))
 
-cnt = 0
+result = abs(100 - n) # 100번 채널부터 n번까지 +/- 버튼으로만 간 경우
 
-def find_n(now):
-    global cnt
+for i in range(1000000): # 큰수 -> 작은수 + 작은수 -> 큰수
+    for j in str(i): # 1번채널부터 n번 채널까지의 최솟값을 다 구한다!
+        if j in broken_number:
+            break
+        if j == len(str(i)) - 1:
+            result = min(result, len(str(i)) + abs(i - n))
 
-    if now == int(n):
-        return cnt
-    else:
+print(result)
